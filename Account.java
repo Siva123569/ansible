5f85dd75d63c20fe5efa6ac5999e5789d03bb52e
Siva123569 <nallasiva552@gmail.com>
2025-04-10 21:23:08 +0530

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,35 @@
+public class Account {
+    private int accountNumber;
+    private double balance;
+    private Customer customer;
+
+    public Account(int accountNumber, Customer customer) {
+        this.accountNumber = accountNumber;
+        this.customer = customer;
+        this.balance = 0.0;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient funds");
+        }
+    }
+}
